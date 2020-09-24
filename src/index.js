8de6bf046f7c7445e03c43f94470eb050ba92194
Чуян Сергей <sergeygrigoryevich@mail.ru>
2020-09-24 15:19:15 +0300

    Refactoring. Removing commented code.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-// import * as $ from "jquery";
-// import Post from "@models/post";
-// import json from "@/assets/json";
-// import xml from "@/assets/text.xml";
-
 import "@/styles/css/styles.css";
 import "@/styles/less/top-line.less";
 import "@/styles/less/slider-fullscreen.less";
@@ -13,15 +8,3 @@ import "@/styles/less/our-services.less";
 import "@/styles/less/contact.less";
 import "@/styles/less/footer.less"
 import "@/sliders.js";
-
-// import "@/slider-fullscreen.js";
-
-// import "@/babel.js"
-// import custImg from "@/assets/settings_filled_icon_153542";
-
-// const post = new Post("Webpack post title", custImg);
-// $("pre").addClass("code").html(post.toString());
-
-// console.log(post.toString());
-// console.log("JSON", json);
-// console.log("XML:", xml);
